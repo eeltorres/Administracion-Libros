2ae1e6f7bddb2b4ffcf4c22776f4411686218f08
Eduardo Torres <eduardo.torres@galileo.edu>
2024-04-05 03:41:34 -0600

    Segunda Entrega
    En esta entrega se perfeccionó el Login y se realizó la recolección de datos como Usuario y Libros.

--- Administracion.cpp
+++ Administracion.cpp
@@ -4,21 +4,22 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
 // Declaracion de Objetos a Utilizar
 struct Libro{
+	int id;
     string titulo;
     string autor;
     string genero;
     int anioPublicacion;
     int numCopiasDisponibles;
-    double nota;
 };
 
 struct Usuario{
-	int idUser;
 	string nameUser;
 };
 
@@ -45,20 +46,33 @@ void clearScreen(){
 	system("cls");
 }
 
+// FUNCION PARA AÑADIR CADA USUARIO
+void aniadirUsuarios(string nameUser);
 
-int idUser = 000;
+// FUNCION PARA OBTENER LOS USUARIOS
+void obtenerUsuarios(vector<Usuario>& usuarios);
+
+
+// FUNCION PARA LEER LOS LIBROS
+void obtenerLibros(vector<Libro>& libros);
+
+// DECLARACION DE VARIABLES GLOBALES
 string nameUser = "";
 
+
+/********************************* FUNCION PRINCIPAL *********************************************************/
 int main() {
 	//Declaracion de variables con scope en el Main
-    string fName, lName, lName2;
+    string fName, lName;
     int op; // variable para la seleccion del usuario
     
+    // CREAR LOS VECTORES DE CADA OBJETO
     vector<Usuario> usuarios;
+    vector<Libro> libros;
     
-    Usuario usuario;
 	
 	inicio:
+	system("cls");
 	cout<<"+++++++++++++  ADMINISTRACION DE BIBLIOTECA +++++++++++++\n\n"<<endl;
 	
     cout << "Ingrese un Nombre" << endl;
@@ -69,53 +83,208 @@ int main() {
 
 	// Validar datos de entrada correctos
     if (f_validarEntrada(nameUser)) {
-        cout << "La entrada no es valida, intentelo nuevamente" << endl;
-		clearScreen();
+    	system("cls");
+        cout << "**** EL NOMBRE O APELLIDO NO ES VALIDO, INTENTE NUEVAMENTE ****" << endl;
+        
+        /* Menu para reintentar loguearse o salir*/
+        cout <<" ------- Presione:\n1. Intentar nuevamente\n0. Salir  -------"<<endl;
+        cin >> op;
+        if(op == 0){
+        	system("cls");
+        	cout<<"Ha salido del programa con exito"<<endl;
+        	exit(1);
+		}else if(op != 1){
+			system("cls");
+			cout<<"***** Seleccion invalida, intente nuevamente *****"<<endl;
+			clearScreen();
+		}
         
         goto inicio;
+		
     } else {
-    	getline(cin, usuario.nameUser);
-    	system("cls");
-    	usuarios.push_back(usuario);
+	    aniadirUsuarios(nameUser);
+	    obtenerUsuarios(usuarios);
     }
     
-    
-    ofstream archivoUsuarios("usuarios.txt");
-    
-    if(!archivoUsuarios.is_open()){
-    	cerr<<"Error al abrir el archivo";
-    	return 1;
-	}
-	
-	for (const Usuario &usuario : usuarios){
-		archivoUsuarios << nameUser << endl;
-	}
-	
-	archivoUsuarios.close();
+
     
     // BIENVENIDA Y ACCESO AL MENU PRINCIPAL
+	menu_principal:
+   	system("cls");
+	
     cout << "Bienvenido: " + nameUser << "\n"<<endl;
-    cout <<"*** Seleccione una opcion: ***\n1. Consulta de un Libro\n2. Prestamo de Libro\n3. Devolucion de Libro\n4. Salir"<<endl;
+    cout <<"*** Seleccione una opcion: ***\n1. Gestion de Libros\n2. Busqueda de Libros\n3. Prestamo o Devolucion\n0. Salir"<<endl;
     cin >> op;
     
     switch(op){
     	case 1:
-    		cout<<"Ha seleccionado consulta"<<endl;
+    		cout<<endl<<"----------------------------------------------------------------------------------------"<<endl;
+    		cout<<"¿Que desea realizar?\n1. Crear Libros\n2. Modificar Libros\n3. Eliminar Libros\n4. Regresar al menu anterior\n0. Cerrar el programa"<<endl;
+    		cin>>op;
+    		switch(op){
+    			case 1:
+    				break;
+    			case 2:
+    				system("cls");
+    				obtenerLibros(libros);
+    				break;
+    			case 3:
+    				break;
+    			case 4:
+    				goto menu_principal;
+    				break;
+    			case 0:
+    				cout<<"Ha salido del programa con exito"<<endl;
+    				exit(1);
+    				break;
+    			default:
+    				cout<<"Seleccion invalida, intente nuevamente"<<endl;
+    				goto menu_principal;
+    				break;
+			}
+
     		break;
     	case 2:
-    		cout<<"Ha seleccionado Prestamo"<<endl;
+    		cout<<endl<<"----------------------------------------------------------------------------------------"<<endl;
+    		cout<<"¿Seleccione el metodo a buscar el libro?\n1. Buscar por titulo\n2. Buscar por autor\n3. Buscar por genero\n4. Buscar por anio"<<endl;
+    		cout<<"5. Regresar al menu anterior\n0. Cerrar el programa"<<endl;
+    		cin>>op;
+    		
+    		switch(op){
+    			case 1:
+    				break;
+    			case 2:
+    				break;
+    			case 3:
+    				break;
+    			case 4:
+    				break;
+    			case 5:
+    				goto menu_principal;
+    				break;
+    			case 0:
+    				cout<<"Ha salido del programa con exito"<<endl;
+    				exit(1);
+    				break;
+    			default:
+    				cout<<"Seleccion invalida, intente nuevamente"<<endl;
+    				system("pause");
+    				goto menu_principal;
+    				break;
+			}
     		break;
     	case 3:
     		cout<<"Ha seleccionado Devolucion"<<endl;
     		break;
-    	case 4:
-    		cout<<"Ha seleccoinado Salir"<<endl;
+    	case 0:
+    		system("cls");
+    		cout<<"*** Ha salido con exito del programa ***"<<endl;
     		break;
     	default:
-    		cout<<"El datos que ingreso es incorrecto"<<endl;
+    		cout<<"El datos que ingreso es incorrecto, intente nuevamente"<<endl;
+    		system("pause");
+			goto menu_principal;
     		break;
 	}
         
     
     return 0;
+}
+
+
+
+
+
+
+void aniadirUsuarios(string nameUser){
+	ofstream archivoUsuarios;
+	
+	archivoUsuarios.open("usuarios.txt", ios::app);
+	
+	if(archivoUsuarios.fail()){
+		cout<<"No se pudo abrir el archivo"<<endl;
+		exit(1);
+	}
+	
+	archivoUsuarios<<nameUser<<endl;
+	
+	archivoUsuarios.close();
+}
+
+
+
+// FUNCION PARA OBTENER USUARIOS
+void obtenerUsuarios(vector<Usuario>& usuarios){
+	ifstream archivoUsuarios;
+	
+	archivoUsuarios.open("usuarios.txt", ios::in);
+	
+	if(archivoUsuarios.fail()){
+		cout<<"No se pudo abrir el archivo usuarios.txt"<<endl;
+		exit(1);
+	}
+	
+	string linea;
+	
+	while(getline(archivoUsuarios, linea)){
+		Usuario usuario;
+		usuario.nameUser = linea;
+		usuarios.push_back(usuario);
+	}
+	archivoUsuarios.close();
+	
+	
+} 
+
+
+// FUNCION PARA OBTENER LIBROS
+void obtenerLibros(vector<Libro>& libros){
+	
+	ifstream archivoLibros;
+	
+	archivoLibros.open("libros.txt", ios::in);
+	
+	if(archivoLibros.fail()){
+		cerr<<"No se pudo abrir el archivo Libros.txt"<<endl;
+		system("pause");
+		exit(1);
+	}
+	string dataLibros;
+	
+	while(getline(archivoLibros, dataLibros)){
+		
+		stringstream ss(dataLibros);
+        string anioPublicacionStr, numCopiasDisponiblesStr, idStr;
+		
+		Libro libro;
+		
+        getline(ss, idStr, ',');
+		getline(ss, libro.titulo, ','); 
+        getline(ss, libro.autor, ','); 
+        getline(ss, libro.genero, ','); 
+		getline(ss, anioPublicacionStr, ',');
+        getline(ss, numCopiasDisponiblesStr); 
+        libro.id = stoi(idStr);
+        libro.anioPublicacion = stoi(anioPublicacionStr); 
+        libro.numCopiasDisponibles = stoi(numCopiasDisponiblesStr);
+        
+        libros.push_back(libro); 
+		
+	}
+	
+ 	cout << "| ID |      TITULO                          |              AUTOR            |       GENERO          | ANIO | NO. DE COPIAS |" << endl;
+    cout << "-------------------------------------------------------------------------------------------------------------------" << endl;
+    for (const Libro& libro : libros) {
+        cout << "| " << setw(3) << libro.id
+             << "| " << setw(39) << libro.titulo
+             << "| " << setw(30) << libro.autor
+             << "| " << setw(24) << libro.genero
+             << "| " << setw(4) << libro.anioPublicacion
+             << "| " << setw(4) << libro.numCopiasDisponibles << " |" << endl;
+    }
+    cout << "--------------------------------------------------------------------------------------------------------------------" << endl;
+    
+    system("pause");
+		archivoLibros.close();
+		
 }
